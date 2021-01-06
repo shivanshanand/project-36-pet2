93a373b7c86fec95142a68d92bd00170abc48a93
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-06 16:56:41 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,24 +1,35 @@
-class Food{
-    constructor(){
-        this.image=milkimg;
+    class Food{
+        constructor(){
+            milkimg=loadImage("Milk.png");
+            milkimg=this.image;
 
-    }
+            this.foodStock=0;
+        }
 
-    display(){
-        var x=80,y=100;
+        display(){
+            var x=80,y=100;
 
-        imageMode(CENTER);
-        image(this.image,720,220,70,70);
+            imageMode(CENTER);
+            image(this.image,720,220,70,70);
 
-        if(this.foodStock!=0){
-            for(var i=0;i<this.foodStock;i=+1){
-                if(i%10==0){
-                    x=80;
-                    y=y+50;
+            if(this.foodStock!=0){
+                for(var i=0;i<this.foodStock;i=+1){
+                    if(i%10==0){
+                        x=80;
+                        y=y+50;
+                    }
+                    image(this.image,x,y,50,50);
+                    x=x+30;
                 }
-                image(this.image,x,y,50,50);
-                x=x+30;
             }
         }
+
+        updateFoodStock(foodStock){
+          this.foodStock=foodStock 
+        }
+
+        getFoodStock(){
+         return this.foodStock;
+        }
+
     }
-}
