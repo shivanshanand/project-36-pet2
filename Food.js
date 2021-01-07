05e20fecd2355f787fd1c755946dbf1af3727c0f
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-07 21:07:47 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -14,7 +14,7 @@
             image(this.milkimg,500,220,70,70);
 
             if(this.foodStock!=0){
-                for(var i=0;i<this.foodStock;i=+1){
+                for(var i=0;i<this.foodStock;i+=1){
                     if(i%10==0){
                         x=80;
                         y=y+50;
